be86e5c4f92189a6fb44c44428302b715809cf9d
William Antony <william@pathadan.com>
2018-03-24 17:21:06 -0500

    Added ModuleName to ModuleNavigation

--- src/components/ModuleNavigation/ModuleNavigation.js
+++ src/components/ModuleNavigation/ModuleNavigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ModuleName from '../ModuleName/ModuleName';
 import './ModuleNavigation.css';
 
 class ModuleNavigation extends Component {
@@ -6,6 +7,7 @@ class ModuleNavigation extends Component {
   render() {
     return (
       <div className="ModuleNavigation">
+        <ModuleName />
         { this.props.children }
       </div>
     );
