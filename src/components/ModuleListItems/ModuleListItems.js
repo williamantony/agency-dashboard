aa28b03a5af973074358853c4784038fc44d9bd1
William Antony <william@pathadan.com>
2018-03-24 16:00:40 -0500

    Created ModuleListItems Component

---
+++ src/components/ModuleListItems/ModuleListItems.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ModuleListItems.css';
+
+class ModuleListItems extends Component {
+
+  render() {
+    return (
+      <div className="ModuleListItems">
+
+      </div>
+    );
+  }
+
+}
+
+export default ModuleListItems;
