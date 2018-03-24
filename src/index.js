00a2fae56217ee5a8328df311f8ecb00550c2912
William Antony <william@pathadan.com>
2018-03-24 10:57:42 -0500

    Added redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import Reducers from './redux/reducers';
 
 import App from './App';
 import './index.css';
 
-const ReduxStore = createStore(Reducers);
+const ReduxStore = createStore(Reducers, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <Provider store={ ReduxStore }>
