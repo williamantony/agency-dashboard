1667f474af6ca5ea7e33aba8543d223b17006e72
William Antony <william@pathadan.com>
2018-03-24 16:00:55 -0500

    Created ModuleItemContent Component

---
+++ src/components/ModuleItemContent/ModuleItemContent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ModuleItemContent.css';
+
+class ModuleItemContent extends Component {
+
+  render() {
+    return (
+      <div className="ModuleItemContent">
+
+      </div>
+    );
+  }
+
+}
+
+export default ModuleItemContent;
