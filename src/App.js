b26b57c2c2d6e97998e1c443c37ecbe613cd0216
William Antony <william@pathadan.com>
2018-03-24 16:01:46 -0500

    Added Module Layout in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import Drawer from './components/Drawer/Drawer';
 import Chat from './views/Chat/Chat';
+import Module from './components/Module/Module';
+import ModuleNavigation from './components/ModuleNavigation/ModuleNavigation';
+import ModuleListItems from './components/ModuleListItems/ModuleListItems';
+import ModuleItemContent from './components/ModuleItemContent/ModuleItemContent';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+
+        <Drawer />
+
+        <Module>
+          <ModuleNavigation />
+          <ModuleListItems />
+          <ModuleItemContent />
+        </Module>
 
         <Route path="/" component={ Chat } />
 
