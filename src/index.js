0639d00f4ca8417cae06d8a174f3ed65c222a2fe
William Antony <william@pathadan.com>
2018-03-24 10:55:22 -0500

    Added redux & react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import Reducers from './redux/reducers';
+
+import App from './App';
+import './index.css';
+
+const ReduxStore = createStore(Reducers);
+
+ReactDOM.render(
+  <Provider store={ ReduxStore }>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
