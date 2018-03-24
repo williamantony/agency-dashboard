8280d82a3ead5802625343160aa008aecfe76ac6
William Antony <william@pathadan.com>
2018-03-24 17:20:22 -0500

    Created ModuleName Component

---
+++ src/components/ModuleName/ModuleName.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './ModuleName.css';
+
+class ModuleName extends Component {
+
+  render() {
+    return (
+      <div className="ModuleName">
+        <div className="ModuleName__text">
+          Inbox
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default ModuleName;
