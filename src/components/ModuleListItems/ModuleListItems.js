808a5b6108395f0708b2b01b815c2bf94f9a8828
William Antony <william@pathadan.com>
2018-03-24 17:10:14 -0500

    Added ModuleSearch Component

--- src/components/ModuleListItems/ModuleListItems.js
+++ src/components/ModuleListItems/ModuleListItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ModuleSearch from '../ModuleSearch/ModuleSearch';
 import './ModuleListItems.css';
 
 class ModuleListItems extends Component {
@@ -6,7 +7,7 @@ class ModuleListItems extends Component {
   render() {
     return (
       <div className="ModuleListItems">
-
+        <ModuleSearch />
       </div>
     );
   }
