ec338449a28a0fbe3f17446a6e124aaff3c8fcda
William Antony <william@pathadan.com>
2018-03-24 21:08:51 -0500

    Added mouse-click-focus to ModuleSearch

--- src/components/ModuleSearch/ModuleSearch.js
+++ src/components/ModuleSearch/ModuleSearch.js
@@ -3,10 +3,20 @@ import './ModuleSearch.css';
 
 class ModuleSearch extends Component {
 
+  focusInputOnClick = (event) => {
+
+    if (event.target.dataset.istarget) {
+      event.target.getElementsByTagName('input')[0].focus();
+    }
+
+  }
+
   render() {
     return (
       <div className="ModuleSearch">
-        <div className="ModuleSearch__Input">
+        <div className="ModuleSearch__Input"
+          data-istarget={ true }
+          onClick={ this.focusInputOnClick } >
           <input type="text" placeholder="Search" />
           <div className="ModuleSearch__Icon" />
         </div>
