d571b5d62bef17c5426d481ce34cfd67a05d4090
William Antony <william@pathadan.com>
2018-03-27 00:21:18 -0500

    Created SidebarLogo Component

---
+++ src/components/SidebarLogo/SidebarLogo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SidebarLogo.css';
+
+class SidebarLogo extends Component {
+
+  render() {
+    return (
+      <div className="SidebarLogo">
+        Brand
+      </div>
+    );
+  }
+
+}
+
+export default SidebarLogo;
