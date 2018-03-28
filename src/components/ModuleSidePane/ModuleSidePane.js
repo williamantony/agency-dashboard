73f447213190a076d6336f9605bdcc8654c3eae9
William Antony <william@pathadan.com>
2018-03-27 22:52:03 -0500

    Modified ModuleSidePane to accept children

--- src/components/ModuleSidePane/ModuleSidePane.js
+++ src/components/ModuleSidePane/ModuleSidePane.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ModuleSearch from '../ModuleSearch/ModuleSearch';
 import './ModuleSidePane.css';
 
 class ModuleSidePane extends Component {
@@ -7,7 +6,7 @@ class ModuleSidePane extends Component {
   render() {
     return (
       <div className="ModuleSidePane">
-        <ModuleSearch />
+      { this.props.children }
       </div>
     );
   }
