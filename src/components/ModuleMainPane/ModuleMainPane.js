f2b0a802f34f2097946eb3d4cb59a6b8369bce1e
William Antony <william@pathadan.com>
2018-03-27 22:44:48 -0500

    Renamed ModuleItemContent to ModuleMainPane

---
+++ src/components/ModuleMainPane/ModuleMainPane.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ModuleMainPane.css';
+
+class ModuleMainPane extends Component {
+
+  render() {
+    return (
+      <div className="ModuleMainPane">
+
+      </div>
+    );
+  }
+
+}
+
+export default ModuleMainPane;
