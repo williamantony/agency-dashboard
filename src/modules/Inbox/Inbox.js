1267f08fd6158c36625b65bea6744160cb07f3e5
William Antony <william@pathadan.com>
2018-03-28 23:35:35 -0500

    Added timestamp for Inbox Email list

--- src/modules/Inbox/Inbox.js
+++ src/modules/Inbox/Inbox.js
@@ -12,6 +12,69 @@ class Inbox extends Component {
       <Module>
         <ModuleSidePane>
           <ModuleSearch />
+          <div className="InboxMessages">
+
+            <div className="InboxMessages__item InboxMessages__item--unread">
+              <div className="InboxMessages__item__content">
+                <div className="InboxMessages__item__icon" />
+                <div className="InboxMessages__item__text">
+                  <div className="line">
+                    <div className="sender">Antony Pathadan</div>
+                    <div className="timestamp">12 mins</div>
+                  </div>
+                  <div className="line line2">
+                    <div className="subject">Request for website quote</div>
+                    <div className="dots">
+                      <div className="dot" />
+                      <div className="dot" />
+                      <div className="dot" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="InboxMessages__item">
+              <div className="InboxMessages__item__content">
+                <div className="InboxMessages__item__icon" />
+                <div className="InboxMessages__item__text">
+                  <div className="line">
+                    <div className="sender">Antony Pathadan</div>
+                    <div className="timestamp">12 mins</div>
+                  </div>
+                  <div className="line line2">
+                    <div className="subject">Request for website quote</div>
+                    <div className="dots">
+                      <div className="dot" />
+                      <div className="dot" />
+                      <div className="dot" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="InboxMessages__item">
+              <div className="InboxMessages__item__content">
+                <div className="InboxMessages__item__icon" />
+                <div className="InboxMessages__item__text">
+                  <div className="line">
+                    <div className="sender">Antony Pathadan</div>
+                    <div className="timestamp">12 mins</div>
+                  </div>
+                  <div className="line line2">
+                    <div className="subject">Request for website quote</div>
+                    <div className="dots">
+                      <div className="dot" />
+                      <div className="dot" />
+                      <div className="dot" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </ModuleSidePane>
 
         <ModuleMainPane />
