e2eac08bebdb90fedbcb7528d7286aaa94eb1ea8
William Antony <william@pathadan.com>
2018-03-29 22:38:19 -0500

    Modified Inbox.js

--- src/modules/Inbox/Inbox.js
+++ src/modules/Inbox/Inbox.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import './Inbox.css';
+
 import Module from '../../components/Module/Module';
 import ModuleSidePane from '../../components/ModuleSidePane/ModuleSidePane';
 import ModuleMainPane from '../../components/ModuleMainPane/ModuleMainPane';
 import ModuleSearch from '../../components/ModuleSearch/ModuleSearch';
+import Conversation from './components/Conversation/Conversation';
+import Messages from './components/Messages/Messages';
 
 class Inbox extends Component {
 
@@ -12,72 +14,12 @@ class Inbox extends Component {
       <Module>
         <ModuleSidePane>
           <ModuleSearch />
-          <div className="InboxMessages">
-
-            <div className="InboxMessages__item InboxMessages__item--unread">
-              <div className="InboxMessages__item__content">
-                <div className="InboxMessages__item__icon" />
-                <div className="InboxMessages__item__text">
-                  <div className="line">
-                    <div className="sender">Mark Zuckerberg</div>
-                    <div className="timestamp">12 mins</div>
-                  </div>
-                  <div className="line line2">
-                    <div className="subject">I was right - and that’s not good for you</div>
-                    <div className="dots">
-                      <div className="dot" />
-                      <div className="dot" />
-                      <div className="dot" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="InboxMessages__item">
-              <div className="InboxMessages__item__content">
-                <div className="InboxMessages__item__icon" />
-                <div className="InboxMessages__item__text">
-                  <div className="line">
-                    <div className="sender">Sheryl Sandberg</div>
-                    <div className="timestamp">45 mins</div>
-                  </div>
-                  <div className="line line2">
-                    <div className="subject">We’re starting in 5 HOURS</div>
-                    <div className="dots">
-                      <div className="dot" />
-                      <div className="dot" />
-                      <div className="dot" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="InboxMessages__item">
-              <div className="InboxMessages__item__content">
-                <div className="InboxMessages__item__icon" />
-                <div className="InboxMessages__item__text">
-                  <div className="line">
-                    <div className="sender">Linus Torvalds</div>
-                    <div className="timestamp">Yesterday</div>
-                  </div>
-                  <div className="line line2">
-                    <div className="subject">It’s time to rethink Black Friday</div>
-                    <div className="dots">
-                      <div className="dot" />
-                      <div className="dot" />
-                      <div className="dot" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
+          <Messages />
         </ModuleSidePane>
 
-        <ModuleMainPane />
+        <ModuleMainPane>
+          <Conversation />
+        </ModuleMainPane>
       </Module>
     );
   }
