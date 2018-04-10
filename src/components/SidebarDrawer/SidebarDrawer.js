16ef848b4686cfcdfb1cca6b3807e5c412ad90e9
William Antony <william@pathadan.com>
2018-04-10 17:29:08 -0500

    Created modules array in state and mapped Links

--- src/components/SidebarDrawer/SidebarDrawer.js
+++ src/components/SidebarDrawer/SidebarDrawer.js
@@ -1,66 +1,65 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './SidebarDrawer.css';
 
 class SidebarDrawer extends Component {
 
+  state = {
+    modules: [
+      {
+        name: 'Inbox',
+        path: '/inbox',
+        icon: '#',
+      },
+      {
+        name: 'Contacts',
+        path: '/contacts',
+        icon: '#',
+      },
+      {
+        name: 'Calendar',
+        path: '/calendar',
+        icon: '#',
+      },
+      {
+        name: 'To do',
+        path: '/todo',
+        icon: '#',
+      },
+      {
+        name: 'Invoices',
+        path: '/invoices',
+        icon: '#',
+      },
+      {
+        name: 'Support',
+        path: '/support',
+        icon: '#',
+      },
+    ]
+  }
+
   render() {
     return (
       <div className="SidebarDrawer">
+        {
+          this.state.modules.map(module => {
+            return (
 
-        <div className="SidebarDrawer__item">
-          <div className="SidebarDrawer__item__icon">
-            <div className="img" />
-          </div>
-          <div className="SidebarDrawer__item__text">
-            Inbox
-          </div>
-        </div>
+              <Link to={ module.path }>
+                <div className="SidebarDrawer__item">
+                  <div className="SidebarDrawer__item__icon">
+                    <div className="img" />
+                  </div>
+                  <div className="SidebarDrawer__item__text">
+                    { module.name }
+                  </div>
+                </div>
+              </Link>
 
-        <div className="SidebarDrawer__item">
-          <div className="SidebarDrawer__item__icon">
-            <div className="img" />
-          </div>
-          <div className="SidebarDrawer__item__text">
-            Contacts
-          </div>
-        </div>
-
-        <div className="SidebarDrawer__item">
-          <div className="SidebarDrawer__item__icon">
-            <div className="img" />
-          </div>
-          <div className="SidebarDrawer__item__text">
-            Calendar
-          </div>
-        </div>
-
-        <div className="SidebarDrawer__item">
-          <div className="SidebarDrawer__item__icon">
-            <div className="img" />
-          </div>
-          <div className="SidebarDrawer__item__text">
-            To do
-          </div>
-        </div>
-
-        <div className="SidebarDrawer__item">
-          <div className="SidebarDrawer__item__icon">
-            <div className="img" />
-          </div>
-          <div className="SidebarDrawer__item__text">
-            Invoices
-          </div>
-        </div>
-
-        <div className="SidebarDrawer__item">
-          <div className="SidebarDrawer__item__icon">
-            <div className="img" />
-          </div>
-          <div className="SidebarDrawer__item__text">
-            Support
-          </div>
-        </div>
-
+            );
+          })
+        }
       </div>
     );
   }
