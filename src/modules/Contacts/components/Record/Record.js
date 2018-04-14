ddf47294f848d9a8940668cdb867d0a8fba3975a
William Antony <william@pathadan.com>
2018-04-14 11:19:33 -0500

    Created Record Component

---
+++ src/modules/Contacts/components/Record/Record.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import RecordEntry from '../RecordEntry/RecordEntry';
+
+import './Record.css';
+
+class Record extends Component {
+
+  render() {
+    return (
+      <div className="Record">
+        <div className="Record__head">
+          <div className="Record__title">{ this.props.record.title }</div>
+        </div>
+        <div className="Record__content">
+        {
+          this.props.record.entries.map((entry, index) => {
+            return <RecordEntry key={ index } entry={ entry } />;
+          })
+        }
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Record;
